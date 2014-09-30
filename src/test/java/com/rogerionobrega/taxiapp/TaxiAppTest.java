532f4c4ccc4fa2c7b977bb61773d6163fb9405b1
Rogerio Nobrega <rogeriocnobrega@gmail.com>
2014-09-30 12:51:22 -0300

    first commit

---
+++ src/test/java/com/rogerionobrega/taxiapp/TaxiAppTest.java
@@ -0,0 +1,19 @@
+package com.rogerionobrega.taxiapp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class TaxiAppTest {
+	
+	public TaxiAppTest(){
+		
+	}
+	
+	@Test
+	public void testarJunitWorking(){
+		assertTrue(true);
+	}
+    
+}
